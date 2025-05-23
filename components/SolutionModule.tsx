9c8ae4674386760a1d3a4168eb1d81ff4a6e5f4d
MATEOPC\wllsl <teoslivka@gmail.com>
2025-05-23 13:38:01 -0700

    fixes api problems

--- components/SolutionModule.tsx
+++ components/SolutionModule.tsx
@@ -6,8 +6,9 @@ import AnswerChoices from './AnswerChoices';
 import { ReactTyped } from 'react-typed';
 import { Question } from '../models/Question';
 import styles from '../assets/styles';
+import { Answer } from '../models/Answer';
 
-const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
+const SolutionModule: React.FC<{ pId: string }> = ({ pId }) => {
     const [loaded, error] = useFonts({
         'Proxima-Nova-Light': require('../assets/fonts/Proxima-Nova/ProximaNova-Light.ttf'),
         'Proxima-Nova-Regular': require('../assets/fonts/Proxima-Nova/ProximaNova-Regular.ttf'),
@@ -17,6 +18,7 @@ const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedOption, setSelectedOption] = useState<string | null>(null);
     const [transcript, setTranscript] = useState(new Map<string, string>());
+    const [answerChoices, setAnswerChoices] = useState<Answer[]>([]);
 
     const handleOptionSelect = (option: string, correctAnswer: string, qIndex: number, qText: string, aText: string) => {
       console.log(`correct answer: ${correctAnswer}\n`);
@@ -28,6 +30,7 @@ const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
           if(option == correctAnswer) {
             console.log(`${aText} a\n`);
             handleNextQuestion(qText, aText);
+            //handleCurrentAnswerChoice(questions[currentQuestionIndex].id);
           }
         }
       };
@@ -35,13 +38,37 @@ const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
       const handleNextQuestion = (qText: string, aText: string) => {
         setSelectedOption(null);
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        //setCurrentQuestionIndex(currentQuestionIndex + 1);
+        //console.log({currentQuestionIndex});
+        
         setTranscript((transcript) => new Map(transcript.set(qText, aText)));
       };
 
+      const handleCurrentAnswerChoice = (qId: string) => {
+        console.log({qId});
+        axios.get<Answer[]>(`https://rt-api-nf0n.onrender.com/answers/question/${qId}`)
+        .then(response => {
+            setAnswerChoices(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching answer data:', error);
+        });
+      }
+
+      useEffect(() => {
+        if(questions.length > 0 && currentQuestionIndex < questions.length) {
+          handleCurrentAnswerChoice(questions[currentQuestionIndex].id);
+        }
+      }, [currentQuestionIndex, questions])
+
     useEffect(() => {
-      axios.get<Question[]>(`https://rt-api-nf0n.onrender.com/problems/solutions/${sId}/questions`)
+      axios.get<Question[]>(`https://rt-api-nf0n.onrender.com/questions/solutions/1`)
       .then(response => {
           setQuestions(response.data);
+          //const qu = questions[0];
+          //handleCurrentAnswerChoice(qu.id);
+          const qu = response.data[0];
+          handleCurrentAnswerChoice(qu.id);
           for (const q of questions) 
           {
             setTranscript((transcript) => new Map(transcript.set(q.content, "")));
@@ -60,15 +87,18 @@ const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
                             <View key={q.id}>
                               {(questionIndex <= currentQuestionIndex) ? (
                                         <View>
+                                          console.log({questionIndex})
+                                          console.log({currentQuestionIndex})
                                         <View style={styles.questionSection}>
                                         <Text style={styles.questionsText}> <ReactTyped strings={[q.content]} typeSpeed={50} showCursor={false}/></Text>
                                       </View>
                                       {(questionIndex < currentQuestionIndex) ? (
                                       <View style={styles.answerChoicesSection}>
+                                        console.log("111");
                                         <RadioGroup>
-                                        {q.answerChoiceIds.map((choice) => (
-                                          <View key={choice}>
-                                            <AnswerChoices current={(questionIndex <= currentQuestionIndex)} answerChoice={choice} cAnswer={q.correctAnswer} index={questionIndex} qText={q.content} onAnswerClick={handleOptionSelect}/> 
+                                        {answerChoices.map((choice) => (
+                                          <View key={choice.id}>
+                                            <AnswerChoices current={(questionIndex <= currentQuestionIndex)} answerChoice={choice.id} cAnswer={q.correctAnswer} index={questionIndex} qText={q.content} onAnswerClick={handleOptionSelect}/> 
                                           </View>
                                         ))}
                                         </RadioGroup>
@@ -76,12 +106,13 @@ const SolutionModule: React.FC<{ sId: string }> = ({ sId }) => {
                                         <br/>
                                         <Text style={styles.questionsText}> <ReactTyped strings={["Correct!"]} typeSpeed={50} showCursor={false}/></Text>
                                       </View>
-                                      ) : (                                    
+                                      ) : (                               
                                       <View style={styles.answerChoicesSection}>
+                                        console.log("000");     
                                         <RadioGroup>
-                                          {q.answerChoiceIds.map((choice) => (
-                                            <View key={choice}>
-                                              <AnswerChoices current={(questionIndex <= currentQuestionIndex)} answerChoice={choice} cAnswer={q.correctAnswer} index={questionIndex} qText={q.content} onAnswerClick={handleOptionSelect}/> 
+                                          {answerChoices.map((choice) => (
+                                            <View key={choice.id}>
+                                              <AnswerChoices current={(questionIndex <= currentQuestionIndex)} answerChoice={choice.id} cAnswer={q.correctAnswer} index={questionIndex} qText={q.content} onAnswerClick={handleOptionSelect}/> 
                                             </View>
                                         ))}
                                         </RadioGroup>                                   
